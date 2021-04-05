7b5b55c9d4488eda51d63d5bfda466658ecc1920
rayasamhr <streamblr.harsh@gmail.com>
2021-04-05 13:39:16 +0800

    Add toolbar

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import KeywordPage from "./components/keywordPage";
 import SummaryPage from "./components/summaryPage";
 import PieChartPage from "./components/pieChartPage";
 import Navbar from "./components/navbar/navbar";
+import Header from "./components/header"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Grid} from "@material-ui/core"
 
 function App() {
   return (
     <div className="App">
         <Router>
             <div>
-                <Navbar/>
-                <Switch>
-                    <Route path="/" exact component={Homepage}/>
-                    <Route path="/meeting" component={MeetingPage}/>
-                    <Route path="/review" component={ReviewPage}/>
-                    <Route path="/keyword" component={KeywordPage}/>
-                    <Route path="/summary" component={SummaryPage}/>
-                    <Route path="/pieChart" component={PieChartPage}/>
-                </Switch>
+                <Grid container direction='column'>
+                  <Header/>
+                  <Switch>
+                      <Route path="/" exact component={Homepage}/>
+                      <Route path="/meeting" component={MeetingPage}/>
+                      <Route path="/review" component={ReviewPage}/>
+                      <Route path="/keyword" component={KeywordPage}/>
+                      <Route path="/summary" component={SummaryPage}/>
+                      <Route path="/pieChart" component={PieChartPage}/>
+                  </Switch>
+                </Grid>
             </div>
         </Router>
     </div>
